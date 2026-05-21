64a9cfaed3acfe3fec479b9ffb0ecdd6189849ca
Isaac Trussell <trussellisaac@meta.com>
2026-05-21 16:46:49 -0700

    remove time collection function.

--- software/control/source/serial_processor.py
+++ software/control/source/serial_processor.py
@@ -144,7 +144,6 @@ class SerialProcessor(ABC, SerialControl):
                 sequence_id += 1
                 if sequence_id > 255:
                     sequence_id = 0
-                self._collect_stats(start_time, send_time, read_time, handle_time)
             except Exception as e:
                 future.set_exception(e)
                 self.logger.error(str(e))
@@ -175,17 +174,6 @@ class SerialProcessor(ABC, SerialControl):
         send = (send_time - start_time) * 1e3
         receive = (read_time - send_time) * 1e3
         return LatencyMs(send=send, receive=receive, total=send + receive)
-
-    def _collect_stats(
-        self, start_time: float, send_time: float, read_time: float, handle_time: float
-    ) -> None:
-        total_time = time.time() - start_time
-        total_send_time = send_time - start_time
-        total_read_time = read_time - send_time
-        total_handle_time = handle_time - read_time
-        self.logger.debug(
-            f"Processing Time: {total_time:.4f} - Send: {total_send_time:.4f} - Read: {total_read_time:.4f} - Handle: {total_handle_time:.4f}"
-        )
 
     def close(self) -> None:
         if not self.queue.empty():
